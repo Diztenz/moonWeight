f1b31afffe1a77f901e27b23c60c28b430be93c1
Diztenz <cosmoc05@yahoo.com>
2021-05-29 15:37:27 -0700

    Initial Commit

---
+++ moonWeight.py
@@ -0,0 +1,8 @@
+# Write your code here :-)
+weight = 109
+moonVar = 0.165
+for i in range(1, 16):
+    total = weight * moonVar
+    print('Year %s weight is %s' % (i, total))
+    weight = weight + 1
+
